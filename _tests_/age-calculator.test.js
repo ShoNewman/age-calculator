5d643b95f22f9283d2287ad930fde45bb07660f3
Shoshana Newman <sho.newman@gmail.com>
2021-06-25 15:19:15 -0700

    Add test for conversion of age to Venus years

--- _tests_/age-calculator.test.js
+++ _tests_/age-calculator.test.js
@@ -3,8 +3,15 @@ import { galacticAge } from "../src/js/age-calculator.js";
 
 describe('Galactic Age Converter', () => {
 
-  test('should test that age is converted to planets years based on age and planet inputted', () => {
-
+  test('should test that age is converted to mercury years', () => {
     expect(galacticAge('mercury', 10)).toEqual(41);
+  });
+
+  test('should test that age is converted to venus years', () => {
+    expect(galacticAge('venus', 10)).toEqual(16);
+  });
+
+  test('should test that age is converted to venus years', () => {
+    expect(galacticAge('venus', 10)).toEqual(16);
   });
 })
